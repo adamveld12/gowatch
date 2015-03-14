37e5c6f3fd440d51c26a2e0cfc0768b911ce1d4e
Adam Veldhousen <adamveld12@gmail.com>
2015-03-13 22:00:00 -0500

    adding different build steps

--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"./exec"
 	"flag"
-	//"gopkg.in/fsnotify.v1"
+	"github.com/adamveld12/goad/exec"
+	"gopkg.in/fsnotify.v1"
 	"log"
 	"path/filepath"
 	"strings"
@@ -16,16 +16,21 @@ import (
  *  go test
  */
 
-var ignore, restartMode, mainFile, cwd string
-var debug, shouldTest, shouldLint bool
+var ignore, mainFile, cwd string
+var debug, shouldTest, shouldLint, restartOnErr, restartOnSave, restartOnBuildErr bool
 
 func init() {
-	flag.BoolVar(&debug, "debug", false, "run in debug mode")
-	flag.BoolVar(&shouldTest, "test", false, "run tests")
-	flag.BoolVar(&shouldLint, "lint", false, "run lint")
-	flag.StringVar(&mainFile, "main", "main.go", "A go file with func main()")
-	flag.StringVar(&ignore, "ignore", "", "Ignores a file path based on the glob specified")
-	flag.StringVar(&restartMode, "restart-mode", "", "How to handle application restarting. error = restart only on error, build = restart only on successful build, save = restart only on save")
+	flag.BoolVar(&debug, "debug", false, "run in debug mode.")
+
+	flag.StringVar(&mainFile, "main", "main.go", "A go file with func main().")
+
+	flag.BoolVar(&shouldTest, "test", false, "Run tests.")
+	flag.BoolVar(&shouldLint, "lint", false, "Run lint.")
+	flag.BoolVar(&restartOnSave, "save", true, "Restart app on save.")
+	flag.BoolVar(&restartOnErr, "error", true, "Restart app if an error occurs during executions.")
+	flag.BoolVar(&restartOnBuildErr, "build", true, "Restart app if an error occurs during compilation.")
+
+	flag.StringVar(&ignore, "ignore", "", "Ignores a file path based on the glob specified.")
 }
 
 func main() {
@@ -40,72 +45,91 @@ func main() {
 	debugLog("main file:", mainFile)
 	debugLog("current working directory:", cwd)
 
-	runSteps()
-
-	//watch(files)
+	if restartOnSave {
+		//watch(files)
+	}
 }
 
-func runSteps() {
+func runSteps() bool {
 
-	build()
+	lintedSuccessfully := !shouldLint
+	if shouldLint {
+		lintedSuccessfully = lint()
+	}
+
+	passedTests := !shouldTest
+	if shouldTest && lintedSuccessfully {
+		passedTests = test()
+	}
+
+	var builtSuccessfully bool = false
+	if lintedSuccessfully && passedTests {
+		builtSuccessfully = build()
+	}
+
+	buildFailed := restartOnBuildErr && (!lintedSuccessfully || !passedTests || !builtSuccessfully)
+
+	restart := restartOnBuildErr && (!lintedSuccessfully || !passedTests || runState.BuildFailed())
+	restart = restart || (restartOnErr && !runState.ExitedSuccessfully())
+
+	return restart
 }
 
-func build() {
+func lint() bool {
+	debugLog("Linting code")
+	return true
+}
+
+func test() bool {
+	debugLog("Testing code")
+	return true
+}
+
+func build() bool {
+	return true
+}
+
+func run() bool {
+	debugLog("Building and execing code")
 	exec.Command("go", "run", mainFile, cwd)
+	return true
 }
 
-// func watch(paths []string) chan string {
-// 	watcher, err := fsnotify.NewWatcher()
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-//
-// 	defer watcher.Close()
-// 	output := make(chan string)
-// 	go func() {
-// 		var err error = nil
-// 		for {
-// 			select {
-// 			case event := <-watcher.Events:
-//
-// 				if debug {
-// 					log.Println("event:", event)
-// 				}
-//
-// 				if event.Op&fsnotify.Write == fsnotify.Write {
-// 					if debug {
-// 						log.Println("modified file:", event.Name)
-// 					}
-// 				}
-//
-// 				err = build()
-// 				for err != nil && (restartMode == "error" || restartMode == "") {
-// 					err = build()
-// 				}
-//
-// 			case err := <-watcher.Errors:
-// 				if debug {
-// 					log.Println("error:", err)
-// 				}
-// 			}
-// 		}
-// 	}()
-//
-// 	err = watcher.Add("./")
-//
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-//
-// 	return output
-// }
+func watch(paths []string) chan string {
+	watcher, err := fsnotify.NewWatcher()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer watcher.Close()
+	signal := make(chan string)
+
+	go func() {
+		for {
+			select {
+			case event := <-watcher.Events:
+				if event.Op&fsnotify.Write == fsnotify.Write {
+					debugLog("modified file:", event.Name)
+					signal <- event.Name
+				}
+			case err := <-watcher.Errors:
+				debugLog("Error:", err.Error())
+			}
+		}
+	}()
+
+	err = watcher.Add("./")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return signal
+}
 
 func debugLog(args ...string) {
 	if debug {
 		log.Println("DEBUG:", strings.Join(args, " "))
 	}
-}
-
-func expandGlobs(globs []string) []string {
-	return []string{}
 }
