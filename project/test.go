22e181a5b0c7c62595ec4b642fd4c18ed1707d6a
Adam Veldhousen <adamveld12@gmail.com>
2015-03-23 22:32:24 -0500

    stubbed out test.go

--- project/test.go
+++ project/test.go
@@ -1,6 +1,11 @@
 package project
 
-import ()
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os/exec"
+)
 
 var testingEnabled bool = false
 
@@ -14,6 +19,26 @@ func Test(mainFile, cwd string) bool {
 	}
 
 	testsPassed := false
+
+	cmd := exec.Command("go", "test", mainFile)
+	cmd.Dir = cwd
+
+	stdout, err := cmd.StdoutPipe()
+	stderr, err := cmd.StderrPipe()
+
+	if err = cmd.Start(); err != nil {
+		log.Fatal(err)
+	}
+
+	stdoutOutput, err := ioutil.ReadAll(stdout)
+	stderrOutput, err := ioutil.ReadAll(stderr)
+
+	if processErr := cmd.Wait(); processErr != nil {
+		fmt.Println(string(stderrOutput))
+		return false
+	}
+
+	fmt.Println(string(stdoutOutput))
 
 	return testsPassed
 }
